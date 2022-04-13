4e7a2318dd21379fead3a65a28a95a8284f1e453
Anant Tiwari <ananttiwariali@gamil.com>
2022-04-13 22:42:17 +0545

    the implementation of major fetures like red for wrong green for right and pop ups

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 console.log('Its inside the quiz javascript file');
-let website= 'https://opentdb.com/api.php?amount=1&category=17&difficulty=easy&type=multiple&token=df8aa604d4a0f947a75a4fe0a101322195d754a18b6a213e982b754ef428215a';
+let website= 'https://opentdb.com/api.php?amount=1&category=17&difficulty=easy&type=multiple';
 
 let question=document.getElementById('question');
-let a= document.getElementById('A')
-let b= document.getElementById('B')
-let c= document.getElementById('C')
-let d= document.getElementById('D')
+let a= document.getElementById('A');
+let b= document.getElementById('B');
+let c= document.getElementById('C');
+let d= document.getElementById('D');
+let container= document.getElementById('show');
 
 var ab;
+var int
+var correctAnswer;
+
+function startIt() {
+    console.log('the restart of the game');
+    console.log('inside the set timeout of restart');
+    question.textContent='question'
+    a.textContent='A:'
+    b.textContent='B:'
+    c.textContent='C:'
+    d.textContent='D:'
+    setTimeout(
+        start(), 1000
+    )
+}
+function rough() {
+    console.log('hi boys it is good rough')
+}
+
+document.onload= start()
 
 a.addEventListener('click', ()=> check(a))
 b.addEventListener('click', ()=> check(b))
@@ -15,13 +36,14 @@ c.addEventListener('click', ()=> check(c))
 d.addEventListener('click', ()=> check(d))
 
 let array;
-let arrayItem=[a,b,c,d];
+var arrayItem=[a,b,c,d];
 
-
-fetch(website)
-        .then((result) => {
-            console.log('We have retrived the quiz data');
-            console.log(result);
+function start() {
+    console.log('the game has started')
+    fetch(website)
+    .then((result) => {
+        console.log('We have retrived the quiz data');
+        console.log(result);
             result.json()
             .then(data=> {
                 array= data;
@@ -32,15 +54,17 @@ fetch(website)
             console.log(err);
             alert('Some error occured see console for more information');
         });
-
-
+        
+        
+    }
 
 
 function getQuestion() {
     question.textContent=array.results[0].question;
-    let int=Math.floor(Math.random()*4);
+    int=Math.floor(Math.random()*4);
     arrayItem[int].textContent+=array.results[0].correct_answer;
-    arrayItem.splice(int,1);
+    correctAnswer= arrayItem.splice(int,1);
+    correctAnswer[0].setAttribute('data-bs-target', '#exampleModal');   //data-bs-target="#exampleModal"
     for (let i = 0; i < arrayItem.length; i++) {
         arrayItem[i].textContent+=array.results[0].incorrect_answers[i];
     }
@@ -49,8 +73,12 @@ function getQuestion() {
 function check(element) {
     console.log(element, 'from the check function');
     ab= element;
+    correctAnswer[0].style.backgroundColor= 'green'
     if (element.textContent=== element.id+':'+array.results[0].correct_answer) {
-        alert('you are right bro oh yo')
+        // alert('you are right bro oh yo');
+
+    }else{
+        ab.style.backgroundColor= 'red';
     }
 } 
 
